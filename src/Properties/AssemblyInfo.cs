e79792b5c321b447b44b37e94a78f2a38cdb28e5
Dorian Stoll <dorian.stoll@tmsp.io>
2019-06-11 19:27:20 +0200

    Add plugin
    Signed-off-by: Dorian Stoll <dorian.stoll@tmsp.io>

---
+++ src/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MyRocksAreBiggerThanYours")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MyRocksAreBiggerThanYours")]
+[assembly: AssemblyCopyright("Copyright © Dorian Stoll 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("C7614371-A231-4889-978C-ED37F146A047")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: KSPAssembly("MyRocksAreBiggerThanYours", 1, 0)]
+[assembly: KSPAssemblyDependency("Kopernicus", 1, 0)]
---
+++ src/Rock.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using Expansions;
+using Kopernicus;
+using Kopernicus.Configuration;
+using UnityEngine;
+
+namespace MyRocksAreBiggerThanYours
+{
+    [KSPAddon(KSPAddon.Startup.Instantly, true)]
+    public class Rock : MonoBehaviour
+    {
+        [SuppressMessage("ReSharper", "ConvertClosureToMethodGroup")]
+        private void Awake()
+        {
+            Events.OnPQSLoaderPostApply.Add((pqs, config) => OnPQSLoaderPostApply(pqs, config));
+        }
+
+        [SuppressMessage("ReSharper", "InconsistentNaming")]
+        private static void OnPQSLoaderPostApply(PQSLoader pqs, ConfigNode node)
+        {
+            // You need to pay for rocks
+            if (!ExpansionsLoader.IsExpansionInstalled("Serenity"))
+            {
+                return;
+            }
+            
+            PQSROCControl rockController = pqs.Value.GetComponentInChildren<PQSROCControl>();
+            if (!rockController)
+            {
+                GameObject rockControllerObject = new GameObject("My rocks are bigger than yours.");
+                rockController = rockControllerObject.AddComponent<PQSROCControl>();
+                rockController.sphere = pqs.Value;
+                rockController.modEnabled = true;
+                rockController.order = 987654321;
+                rockController.transform.parent = rockController.sphere.transform;
+                rockController.rocs = new List<LandClassROC>();
+            }
+
+            // We support modding
+            rockController.currentCBName = pqs.Value.name;
+        }
+    }
+}
